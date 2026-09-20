f971ef1b1fb9b35b67d4c8b70541b9ccbc333191
rajdeep-11 <rajdeepmandal841@gmail.com>
2026-09-20 12:00:22 +0530

    added jwt create and varify tokens

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { SERVERCONFIG } = require('./config');
 const apiRoutes = require('./routes');
 
 //const { UserRepository } = require('./repositories')
+const { UserService } = require('./services')
 
 const serverPort = SERVERCONFIG.PORT;
 
@@ -24,6 +25,15 @@ const prepareAndStartServer = () => {
         // const userRepo = new UserRepository;
         // const response = await userRepo.getById(3);
         // console.log(response);
+
+        // const userService = new UserService();
+        // // const newToken = userService.createToken({email:'raj@google.com', id: 2});
+        // // console.log('new token', newToken);
+
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhakBnb29nbGUuY29tIiwiaWQiOjIsImlhdCI6MTc4OTg4NTU1MCwiZXhwIjoxNzg5ODg1NTgwfQ.m7RnRZrW1VAVEeBSSm7MASb-ft5Ah-3V9L43CKn8Qvw';
+
+        // const verifyNewToken = userService.verifyToken(token)
+        // console.log(verifyNewToken);
     });
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
 const { UserRepository } = require('../repositories');
+const jwt = require('jsonwebtoken');
+const { SERVERCONFIG } = require('../config');
+const { JWT_KEY } = require('../config/server-config');
 
 class UserService {
     constructor() {
@@ -11,6 +14,26 @@ class UserService {
             return user;
         } catch (error) {
             console.log('Something went wrong in service layer');
+            throw error;
+        }
+    }
+
+    createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
+            return result;
+        } catch (error) {
+            console.log('Something went wrong in jwt creation');
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log('Something went wrong in jwt verification', error);
             throw error;
         }
     }
